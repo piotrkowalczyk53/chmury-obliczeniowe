654779fc7902da0d1c52d20f596afcc2c9c3f6ae
Piotr Kowalczyk <92885222+piotrkowalczyk53@users.noreply.github.com>
2023-12-14 19:58:08 +0100

    Almost finished

--- FamilyTree.py
+++ FamilyTree.py
@@ -14,33 +14,34 @@ class FamilyTree:
     def get_family_tree(self):
         with self._driver.session() as session:
             result = session.run("""
-                MATCH (n), ()-[r]->()
-                RETURN COLLECT(distinct n) AS nodes,
-                COLLECT(distinct r) AS relationships
+                MATCH (n:Person)
+                OPTIONAL MATCH (n:Person)-[r]->()
+                RETURN COLLECT(DISTINCT n) AS nodes, COLLECT(DISTINCT r) AS relationships
                 """)
-
             nodes = []
             edges = []
             for record in result:
                 for node in record['nodes']:
-                    nodes.append({'id': node['id'], 'label': node['last_name'] + " " +  node['first_name']})
+                    nodes.append({'id': node.id, 'label': node['last_name'] + " " + node['first_name']})
 
                 for relationship in record['relationships']:
                     start_node = relationship.start_node
                     end_node = relationship.end_node
                     rel_type = relationship.type.lower()
-                    edges.append({'from': start_node['id'], 'to': end_node['id'], 'relation': rel_type, 'arrows': 'middle'})
+                    edges.append({'from': start_node.id, 'to': end_node.id, 'relation': rel_type, 'arrows': 'middle'})
 
             return nodes, edges
+
         
     def get_person_info(self, person_id):
         with self._driver.session() as session:
             result = session.run("""
-                MATCH (person:Person {id: $person_id})
+                MATCH (person:Person)
+                WHERE id(person) = $person_id
                 OPTIONAL MATCH (person)-[:FATHER|MOTHER]->(child)
                 OPTIONAL MATCH (person)<-[:FATHER|MOTHER]-(parent)
-                OPTIONAL MATCH (person)-[:HUSBAND|WIFE]->(spouse)
-                RETURN person, COLLECT(DISTINCT child) AS children, COLLECT(parent) AS parents, COLLECT(spouse) AS spouses
+                OPTIONAL MATCH (person)-[:SPOUSE]->(spouse)
+                RETURN person, COLLECT(child) AS children, COLLECT(parent) AS parents, COLLECT(spouse) AS spouses
                 """, person_id=person_id)
 
             person_info = {}
@@ -49,44 +50,282 @@ class FamilyTree:
                 person_info['children'] = record['children']
                 person_info['parents'] = record['parents']
                 person_info['spouses'] = record['spouses']
-            
+
             if len(person_info.get('children', [])) == 0:
                 del person_info['children']
                 
             if len(person_info.get('parents', [])) == 0:
                 del person_info['parents']
+                
+            if len(person_info.get('spouses', [])) == 0:
+                del person_info['spouses']
             
             return person_info
+    
+    def get_person(self, person_id):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (person:Person)
+                WHERE id(person) = $person_id
+                RETURN person AS person
+                """, person_id=person_id)
+
+            record = result.single()
+            return record['person']
         
-    def add_child(self, Person):
-        with db.session() as session:
-            session.run(
-                "CREATE (p:Person {id: $id, first_name: $first_name, last_name: $last_name, gender: $gender, birth: $birth, death: $death})",
-                id=new_child.id,
-                first_name=new_child.first_name,
-                last_name=new_child.last_name,
-                gender=new_child.gender,
-                birth=new_child.birth,
-                death=new_child.death
-            )
+    def edit_person(self, person_id, first_name, last_name, gender, birth, death):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (person:Person)
+                WHERE id(person) = $person_id
+                SET person.first_name = $first_name,
+                    person.last_name = $last_name,
+                    person.gender = $gender,
+                    person.birth = date($birth),
+                    person.death = date($death)
+                RETURN person
+                """, person_id=person_id, first_name=first_name, last_name=last_name,
+                                 gender=gender, birth=birth, death=death)
             
+    def delete_person(self, person_id):
+        with self._driver.session() as session:
             session.run(
-                "MATCH (parent:Person {id: $parent_id}), (child:Person {id: $child_id}) CREATE (parent)-[:PARENT_OF]->(child)",
-                parent_id=parent_id,
-                child_id=new_child.id
-            )
-            
-            session.run(
-                "MATCH (parent:Person {id: $parent_id}), (child:Person {id: $child_id}) CREATE (parent)-[:PARENT_OF]->(child) CREATE (child)-[:CHILD_OF]->(parent)",
-                parent_id=parent_id,
-                child_id=new_child.id
+                """
+                MATCH (person:Person)
+                WHERE id(person) = $person_id
+                DETACH DELETE person
+                """,
+                person_id=person_id,
             )
         
-class Person:
-    def __init__(self, id, first_name, last_name, gender, birth, death):
-        self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.gender = gender
-        self.birth = birth
-        self.death = death
+    def get_all_males(self):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (person:Person {gender: 'Male'})
+                RETURN person
+                """)
+            
+            nodes = [record['person'] for record in result]
+            return nodes
+        
+    def get_all_females(self):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (person:Person {gender: 'Female'})
+                RETURN person
+                """)
+            
+            nodes = [record['person'] for record in result]
+            return nodes
+            
+    def get_all_dead(self):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (person:Person)
+                WHERE person.death < date()
+                RETURN person
+                """)
+        
+            nodes = [record['person'] for record in result]
+            return nodes
+            
+    def get_all_alive(self):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (person:Person)
+                WHERE person.death IS NULL OR person.death = ''
+                RETURN person
+                """)
+            
+            nodes = [record['person'] for record in result]
+            return nodes
+        
+    def get_all_marriages(self):
+        with self._driver.session() as session:
+            result = session.run("""
+                MATCH (h:Person {gender: "Male"})-[marriage:MARRIAGE]->(w:Person)
+                RETURN COLLECT(DISTINCT [h, w, marriage]) AS marriages
+            """)
+            
+            marriages = [record['marriages'][0] for record in result if record['marriages']]
+            return marriages if marriages else []
+        
+    def add_person(self, first_name, last_name, gender, birth, death):
+        with self._driver.session() as session:
+            result = session.run("""
+                CREATE (p:Person {
+                first_name: $first_name,
+                last_name: $last_name,
+                gender: $gender,
+                birth: date($birth),
+                death: date($death)
+            })
+            RETURN id(p) AS person_id
+            """, first_name=first_name, last_name=last_name, gender=gender, birth=birth, death=death)
+
+            return result.single()['person_id']
+        
+    def add_child(self, person_id, first_name, last_name, gender, birth, death, parent_gender):
+        with self._driver.session() as session:
+            relationship_type = "FATHER" if parent_gender == "Male" else "MOTHER"
+
+            query = """
+                MATCH (parent:Person)
+                WHERE id(parent) = $person_id
+                CREATE (child:Person {
+                    first_name: $first_name,
+                    last_name: $last_name,
+                    gender: $gender,
+                    birth: date($birth)
+                    %s
+                })
+                CREATE (parent)-[:%s]->(child)
+                CREATE (parent)<-[:%s]-(child)
+                """ % (", death: date($death)" if death else "", relationship_type, "DAUGHTER" if gender == "Female" else "SON")
+
+            parameters = {
+                "person_id": person_id,
+                "first_name": first_name,
+                "last_name": last_name,
+                "gender": gender,
+                "birth": birth,
+                "death": death
+            }
+
+            session.run(query, **parameters)
+
+            return person_id
+        
+    def add_parent(self, person_id, first_name, last_name, gender, birth, death, child_gender):
+        with self._driver.session() as session:
+            relationship_type = "SON" if child_gender == "Male" else "DAUGHTER"
+            parent_relationship = "MOTHER" if gender == "Female" else "FATHER"
+
+            query = """
+                    MATCH (child:Person)
+                    WHERE id(child) = $person_id
+
+                    OPTIONAL MATCH (old_parent:Person)-[old_relation:%s]->(child:Person)
+                    WHERE id(child) = $person_id
+                    DELETE old_relation
+
+                    CREATE (parent:Person {
+                        first_name: $first_name,
+                        last_name: $last_name,
+                        gender: $gender,
+                        birth: date($birth)
+                        %s
+                    })
+                    CREATE (child)-[:%s]->(parent)
+                    CREATE (child)<-[:%s]-(parent)
+                    """ % (
+                        parent_relationship,
+                        ", death: date($death)" if death else "",
+                        relationship_type,
+                        parent_relationship
+                    )
+
+            parameters = {
+                "person_id": person_id,
+                "first_name": first_name,
+                "last_name": last_name,
+                "gender": gender,
+                "birth": birth,
+                "death": death
+            }
+
+            session.run(query, **parameters)
+
+            return person_id
+        
+    def add_spouse(self, person_id, first_name, last_name, gender, birth, death):
+        with self._driver.session() as session:
+            query = """
+                    MATCH (me:Person)
+                    WHERE id(me) = $person_id
+
+                    OPTIONAL MATCH (ex:Person)-[divorce:MARRIAGE]-(me:Person)
+                    WHERE id(me) = $person_id
+                    SET divorce.divorce_date = date(datetime())
+
+                    CREATE (new:Person {
+                        first_name: $first_name,
+                        last_name: $last_name,
+                        gender: $gender,
+                        birth: date($birth)
+                        %s
+                    })
+
+                    CREATE (me)-[new_m:MARRIAGE]->(new)
+                    CREATE (me)<-[new_m2:MARRIAGE]-(new)
+                    SET new_m.marriage_date = date(datetime())
+                    SET new_m2.marriage_date = date(datetime())
+                    """ % (
+                        ", death: date($death)" if death else ""
+                    )
+
+            parameters = {
+                "person_id": person_id,
+                "first_name": first_name,
+                "last_name": last_name,
+                "gender": gender,
+                "birth": birth,
+                "death": death
+            }
+
+            session.run(query, **parameters)
+
+            return person_id
+
+    def delete_relation(self, parent_id, child_id):
+        with self._driver.session() as session:
+            relation_query = """
+                MATCH (parent:Person)-[rel]->(child:Person)
+                WHERE id(parent) = $parent_id AND id(child) = $child_id
+                DELETE rel
+
+                MATCH (parent:Person)-[rel]-(child:Person)
+                WHERE id(parent) = $parent_id AND id(child) = $child_id
+                DELETE rel
+            """
+
+            session.run(relation_query, parent_id=parent_id, child_id=child_id)
+
+    def create_relationship(self, node1, node2, relationship, gender2):
+        with self._driver.session() as session:
+            if relationship == 'FATHER' or relationship == 'MOTHER':
+                if gender2 == 'male':
+                    relationship2 = 'SON'
+                elif gender2 == 'female':
+                    relationship2 = 'DAUGHTER'
+
+                session.run("""
+                    MATCH (n1:Person), (n2:Person)
+                    WHERE id(n1) = $node1_id AND id(n2) = $node2_id
+                    CREATE (n1)-[:%s]->(n2)
+                    CREATE (n1)<-[:%s]-(n2)
+                """, relationship, relationship2, node1_id=node1, node2_id=node2)
+
+            elif relationship == 'MARRIAGE':
+                session.run("""
+                    MATCH (n1:Person), (n2:Person)
+                    WHERE ID(n1) = $node1_id AND ID(n2) = $node2_id
+                            
+                    OPTIONAL MATCH (n1)-[m1:MARRIAGE]->(n2)
+                    WHERE m1.divorce_date IS NULL
+                    SET m1.divorce_date = date(datetime())
+                    WITH n1, n2, m1
+                            
+                    OPTIONAL MATCH (n2)-[m2:MARRIAGE]->(n1)
+                    WHERE m2.divorce_date IS NULL
+                    SET m2.divorce_date = date(datetime())
+                    WITH n1, n2, m1, m2
+                            
+                    WHERE m1 IS NULL AND m2 IS NULL
+                    CREATE (n1)-[newm1:MARRIAGE]->(n2)
+                    SET newm1.marriage_date = date(datetime())
+                    WITH n1, n2, newm1
+                            
+                    CREATE (n2)-[newm2:MARRIAGE]->(n1)
+                    SET newm2.marriage_date = date(datetime())
+                """, node1_id=node1, node2_id=node2)
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from FamilyTree import *
 
 app = Flask(__name__)
@@ -20,26 +20,126 @@ def display_family_tree():
 @app.route('/person/<int:person_id>')
 def display_person(person_id):
     person_info = family_tree.get_person_info(person_id)
-
     return render_template('person.html', data=person_info)
 
-@app.route("/add_child", methods=['POST'])
-def add_child():
-    id = request.form.get('id')
-    first_name = request.form.get('first_name')
-    last_name = request.form.get('last_name')
-    gender = request.form.get('gender')
-    birth = request.form.get('birth')
-    death = request.form.get('death')
-    parent_id = request.form.get('parent_id')
+@app.route('/males')
+def get_all_males():
+    male_nodes = family_tree.get_all_males()
+    return render_template('people.html', people=male_nodes)
 
-    new_child = Person(id, first_name, last_name, gender, birth, death)
+@app.route('/females')
+def get_all_females():
+    female_nodes = family_tree.get_all_females()
+    return render_template('people.html', people=female_nodes)
+
+@app.route('/dead')
+def get_all_dead():
+    dead_nodes = family_tree.get_all_dead()
+    return render_template('people.html', people=dead_nodes)
+
+@app.route('/alive')
+def get_all_alive():
+    alive_nodes = family_tree.get_all_alive()
+    return render_template('people.html', people=alive_nodes)
+
+@app.route('/marriages')
+def get_all_married():
+    married_nodes = family_tree.get_all_marriages()
+    return render_template('marriages.html', marriages=married_nodes)
+
+@app.route('/people/add', methods=('GET', 'POST'))
+def person_add():
+	if request.method == 'POST':
+		first_name = request.form['firstName']
+		last_name = request.form['lastName']
+		gender = request.form['gender']
+		birth = request.form['birth']
+		death = request.form['death']
+		id = family_tree.add_person(first_name, last_name, gender, birth, death)
+		return redirect(f'/person/{id}')
+
+	return render_template('person_add.html')
+
+
+@app.route('/person/<int:id>/edit', methods=('GET', 'POST'))
+def person_edit(id):
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        gender = request.form['gender']
+        birth = request.form['birth']
+        death = request.form['death']
+        family_tree.edit_person(id, first_name, last_name, gender, birth, death)
+        return redirect(f'/person/{id}')
+
+    person = family_tree.get_person(id)
+    return render_template('person_edit.html', person=person)
+
+@app.route('/person/<int:id>/delete')
+def person_delete(id):
+    family_tree.delete_person(id)
+    return redirect("/")
+
+@app.route('/person/<int:id>/add_child', methods=('GET', 'POST'))
+def child_add(id):
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        gender = request.form['gender']
+        birth = request.form['birth']
+        death = request.form['death']
+        parent_gender = request.form['parent_gender']
+        child_id = family_tree.add_child(id, first_name, last_name, gender, birth, death, parent_gender)
+        return redirect(f'/person/{child_id}')
+
+    parent_gender = request.args.get('parent_gender')
+    return render_template('person_add.html', parent_gender=parent_gender)
+
+@app.route('/delete_relation/<int:parent_id>/<int:child_id>', methods=['POST'])
+def delete_relation(parent_id, child_id):
+    if request.method == 'POST':
+        family_tree.delete_relation(parent_id, child_id)
+        return redirect(f'/person/{parent_id}')
+
+@app.route('/person/<int:id>/add_parent', methods=('GET', 'POST'))
+def parent_add(id):
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        gender = request.form['gender']
+        birth = request.form['birth']
+        death = request.form['death']
+        child_gender = request.form['parent_gender']
+        child_id = family_tree.add_parent(id, first_name, last_name, gender, birth, death, child_gender)
+        return redirect(f'/person/{child_id}')
+
+    child_gender = request.args.get('child_gender')
+    return render_template('person_add.html', parent_gender=child_gender)
+
+@app.route('/person/<int:id>/add_spouse', methods=('GET', 'POST'))
+def spouse_add(id):
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        gender = request.form['gender']
+        birth = request.form['birth']
+        death = request.form['death']
+        child_id = family_tree.add_spouse(id, first_name, last_name, gender, birth, death)
+        return redirect(f'/person/{child_id}')
+
+    child_gender = request.args.get('child_gender')
+    return render_template('person_add.html', parent_gender=child_gender)
+
+@app.route('/create_relationship/<int:node1>/<int:node2>', methods=['GET', 'POST'])
+def create_relationship(node1, node2):
+    if request.method == 'POST':
+        relationship = request.form['relationship']
+        gender2 = request.form['node2_gender']
+        family_tree.create_relationship(node1, node2, relationship, gender2)
+        return redirect('/')
     
-    family_tree.add_child(new_child)
-    
-    nodes, edges = family_tree.get_family_tree()
-
-    return render_template('index.html', nodes=nodes, edges=edges)
-
+    person1 = family_tree.get_person(node1)
+    person2 = family_tree.get_person(node2)
+    return render_template('create_relationship.html', person1=person1, person2=person2)
 
 if __name__ == '__main__': app.run(debug=True)
